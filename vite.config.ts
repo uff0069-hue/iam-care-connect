7f96b3ebed4cf35e6a8d1f063c5380085507b463
RajatThakur <uff0069@gmail.com>
2026-08-08 22:00:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/iam-care-connect/",
-  },
   tanstackStart: {
     server: { entry: "server" },
   },
